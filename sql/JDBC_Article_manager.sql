5f6abd16e0df92116594d7bf3ace7cf062d28d72
Hyunfor <navel5148@gmail.com>
2022-10-12 20:08:13 +0900

    회원가입 기능 구현

--- sql/JDBC_Article_manager.sql
+++ sql/JDBC_Article_manager.sql
@@ -2,6 +2,10 @@ DROP DATABASE IF EXISTS jdbc_article_manager;
 CREATE DATABASE jdbc_article_manager;
 USE jdbc_article_manager;
 
+DROP TABLE IF EXISTS `member`;
+DROP TABLE IF EXISTS article;
+
+# article 테이블 생성
 CREATE TABLE article(
 	id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	regDate DATETIME NOT NULL,
@@ -10,15 +14,49 @@ CREATE TABLE article(
 	`body` TEXT NOT NULL
 );
 
+# 구조확인
 DESC article;
 
-SELECT * FROM article
+# member 테이블 생성
+CREATE TABLE `member` (
+	id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	regDate DATETIME NOT NULL,
+	updateDate DATETIME NOT NULL,
+	loginId VARCHAR(20) NOT NULL,
+	loginPw VARCHAR(50) NOT NULL,
+	loginPwC VARCHAR(50) NOT NULL,
+	`name` VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE `member` (
+	id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	regDate DATETIME NOT NULL,
+	updateDate DATETIME NOT NULL,
+	loginId VARCHAR(20) NOT NULL,
+	loginPw VARCHAR(50) NOT NULL,
+	`name` VARCHAR(50) NOT NULL
+);
 
 #역순 체크
-SELECT * FROM article ORDER BY id DESC
+SELECT * FROM article ORDER BY id DESC;
 
+# article 데이터 추가
 INSERT INTO article
 SET regDate = NOW(),
 updateDate = NOW(),
 title = CONCAT('제목', RAND()),
 `body` = CONCAT('내용', RAND());
+
+# member 데이터 추가
+INSERT INTO `member`
+SET regDate = NOW(),
+updateDate = NOW(),
+loginId = CONCAT('TestId', RAND()),
+loginPw = CONCAT('TestPw', RAND()),
+`name` = CONCAT('Testname', RAND());
+
+# article 테이블 조회
+SELECT * FROM article;
+
+# member 테이블 조회
+SELECT * FROM `member`;
