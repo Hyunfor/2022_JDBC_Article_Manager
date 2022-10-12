b6b9f80faaf8e5a97d10d0495d09666b909e6f5b
Hyunfor <navel5148@gmail.com>
2022-10-12 20:26:19 +0900

    회원가입 시 로그인 아이디 중복체크

--- src/com/KoreaIT/example/JAM/App.java
+++ src/com/KoreaIT/example/JAM/App.java
@@ -72,10 +72,25 @@ public class App {
 				System.out.printf("아이디 : ");
 				loginId = sc.nextLine().trim();
 
-				if(loginId.length() == 0) {
+				if(loginId.length() == 0) { 
 					System.out.println("아이디를 입력해주세요");
 					continue;
 				}
+				
+				// 아이디 중복체크 쿼리
+				SecSql sql = new SecSql();
+				
+				sql.append("SELECT COUNT(loginId) > 0");
+				sql.append("FROM `member`");
+				sql.append("WHERE loginId = ?", loginId);
+				
+				boolean isLoginIdDup =  DBUtil.selectRowBooleanValue(conn, sql);
+				
+				if(isLoginIdDup) { // 중복된 아이디가 있는 경우
+					System.out.printf("%s은(는) 이미 사용중인 아이디입니다.\n", loginId);
+					continue;
+				}
+				
 				break;
 			}
 			while(true) {
@@ -129,9 +144,9 @@ public class App {
 			sql.append(", loginPw = ?", loginPw);
 			sql.append(", `name` = ?", name);
 			
-			int id = DBUtil.insert(conn, sql);
+			DBUtil.insert(conn, sql);
 
-			System.out.printf("%s회원님, 가입 되었습니다\n", name);
+			System.out.printf("%s 회원님, 가입 되었습니다\n", name);
 
 		} else if (cmd.equals("article write")) {
 				System.out.println("== 게시물 작성 ==");
