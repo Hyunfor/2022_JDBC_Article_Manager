872d427cb00925bc503fd78ca6f46fcce481ae0a
Hyunfor <navel5148@gmail.com>
2022-10-19 20:46:26 +0900

    member profile 기능 추가

--- src/com/KoreaIT/example/JAM/controller/Controller.java
+++ src/com/KoreaIT/example/JAM/controller/Controller.java
@@ -2,6 +2,7 @@ package com.KoreaIT.example.JAM.controller;
 
 import java.util.Scanner;
 
+import com.KoreaIT.example.JAM.Member;
 import com.KoreaIT.example.JAM.container.Container;
 
 public class Controller {
@@ -10,4 +11,12 @@ public class Controller {
 	public Controller() {
 		this.sc = Container.sc;
 	}
+	
+	public static Member loginedMember; // Article과 Member공유재로 만들어두기
+	
+	public static boolean isLogined() {
+		
+		return loginedMember != null;
+	}
+	
 }
